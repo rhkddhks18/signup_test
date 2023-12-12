6199143b32b22565338156f3593d51549f95fe84
ko <139303842+rhkddhks18@users.noreply.github.com>
2023-12-13 01:38:21 +0900

    first commit

---
+++ src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password, String area, String level) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setArea(area);
+        user.setLevel(level);
+        this.userRepository.save(user);
+        return user;
+    }
+}
